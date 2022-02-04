c237e2520168be7feb8c4d839184d72223785a69
longcv1 <longcv1.khmt@gmail.com>
2022-02-04 21:37:12 +0700

    Add project files.

---
+++ Application/Main.cpp
@@ -0,0 +1,33 @@
+#include<iostream>
+
+int main()
+{
+   using namespace std;
+   cout << "\n******** DESIGN PATTERN USING C++ ********\n";
+   cout << "\t=> Creational Patterns:\n";
+   cout << "\t\t1. Builder\n";
+   cout << "\t\t2. Factory Method / Abstract Factory\n";
+   cout << "\t\t3. Prototype\n";
+   cout << "\t\t4. Singleton\n";
+   cout << "\t=> Structure Patterns:\n";
+   cout << "\t\t1. Adapter\n";
+   cout << "\t\t2. Bridge\n";
+   cout << "\t\t3. Composite\n";
+   cout << "\t\t4. Decorator\n";
+   cout << "\t\t5. Facade\n";
+   cout << "\t\t6. Flyweight\n";
+   cout << "\t\t7. Proxy\n";
+   cout << "\t=> Behavior Patterns:\n";
+   cout << "\t\t1.Chain of Responsibility \n";
+   cout << "\t\t2.Command \n";
+   cout << "\t\t3.Iterator \n";
+   cout << "\t\t4.Mediator \n";
+   cout << "\t\t5.Memento \n";
+   cout << "\t\t6.Observer \n";
+   cout << "\t\t7.State \n";
+   cout << "\t\t8.tategy \n";
+   cout << "\t\t9.Template Method \n";
+   cout << "\t\t10.Visitor \n";
+   return 0;
+}
+
